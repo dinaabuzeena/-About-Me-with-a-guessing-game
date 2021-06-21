c751fdd77966323a56a60a119c9e4ec1e55d8e86
dinaabuzeena <dinamohammad735@gmail.com>
2021-06-21 16:27:31 +0300

    edit

---
+++ js/app.js
@@ -0,0 +1,82 @@
+'use strict'
+
+let theName=  prompt ("what is your Name?")
+console .log("welcome"+ theName +"to my webpage")
+alert("welcome"+ theName +"to my webpage")
+
+
+let myPhone= prompt("is my phone Iphone?")
+switch(myPhone.toLowerCase()){
+case'yes':
+case'y':
+console .log(myPhone)
+alert("my phone is Iphone")
+break;
+case'no':
+case'n':
+console .log(myPhone)
+alert("your answer is wronge")
+break;
+
+}
+
+
+let myFavoriteColor= prompt("is my favorite color white?")
+switch(myFavoriteColor.toLowerCase()){
+case'yes':
+case'y':
+console .log(myFavoriteColor)
+alert("my favorite color is  white")
+break;
+case'no':
+case'n':
+console .log(myFavoriteColor)
+alert("your answer is wronge")
+break;
+}
+
+
+
+let myCar= prompt("is my car BMW?")
+switch(myCar.toLowerCase()){
+case'yes':
+case'y':
+console .log( myCar)
+alert("my car is BMW")
+break;
+case'no':
+case'n':
+console .log(myCar)
+alert("your answer is wronge")
+break;
+}
+
+
+let myFavoriteMovie= prompt("is my favorite movie froozen?")
+switch(myFavoriteMovie.toLowerCase()){
+case'yes':
+case'y':
+console .log(myFavoriteMovie)
+alert("my favorite movie is froozen")
+break;
+case'no':
+case'n':
+console .log(myFavoriteMovie)
+alert("your answer is wronge")
+break;
+}
+
+
+let myAge= prompt("is my age above 20 years?")
+switch(myAge.toLowerCase()){
+case'yes':
+case'y':
+console .log(myAge)
+alert("my age above 20 years")
+break;
+case'no':
+case'n':
+console .log(myAge)
+alert("your answer is wronge")
+break;
+}
